f0e8e36b0e03c0f144199abd400479bbbe1689cd
WujieRen <rwj005@outlook.com>
2023-04-06 22:03:33 +0800

    新增 common 模块

---
+++ common/src/main/java/cn/rwj/study/common/App.java
@@ -0,0 +1,13 @@
+package cn.rwj.study.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
