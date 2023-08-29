da90d699fe67541158d2015fbe4f56028c0d7ae5
rwj <rwj005@outlook.com>
2023-08-29 11:10:53 +0800

    chore: Review, add some annotation.

--- flink/src/main/java/cn/rwj/study/flink/connector/http/HttpDynamicTableFactory.java
+++ flink/src/main/java/cn/rwj/study/flink/connector/http/HttpDynamicTableFactory.java
@@ -4,11 +4,13 @@ import org.apache.flink.api.common.serialization.DeserializationSchema;
 import org.apache.flink.configuration.ConfigOption;
 import org.apache.flink.configuration.ConfigOptions;
 import org.apache.flink.configuration.ReadableConfig;
+import org.apache.flink.table.catalog.CatalogTable;
 import org.apache.flink.table.connector.format.DecodingFormat;
 import org.apache.flink.table.connector.source.DynamicTableSource;
 import org.apache.flink.table.data.RowData;
 import org.apache.flink.table.factories.DeserializationFormatFactory;
 import org.apache.flink.table.factories.DynamicTableSourceFactory;
+import org.apache.flink.table.factories.Factory;
 import org.apache.flink.table.factories.FactoryUtil;
 import org.apache.flink.table.types.DataType;
 
@@ -33,11 +35,24 @@ public class HttpDynamicTableFactory implements DynamicTableSourceFactory {
             .longType()
             .noDefaultValue();
 
+    /**
+     * Returns a unique identifier among same factory interfaces.
+     *
+     * <p>For consistency, an identifier should be declared as one lower case word (e.g. {@code
+     * kafka}). If multiple factories exist for different versions, a version should be appended
+     * using "-" (e.g. {@code elasticsearch-7}).
+     */
     @Override
     public String factoryIdentifier() {
         return "http"; // used for matching to `connector = '...'`
     }
 
+    /**
+     * Returns a set of {@link ConfigOption} that an implementation of this factory requires in
+     * addition to {@link #optionalOptions()}.
+     *
+     * <p>See the documentation of {@link Factory} for more information.
+     */
     @Override
     public Set<ConfigOption<?>> requiredOptions() {
         final Set<ConfigOption<?>> options = new HashSet<>();
@@ -47,6 +62,12 @@ public class HttpDynamicTableFactory implements DynamicTableSourceFactory {
         return options;
     }
 
+    /**
+     * Returns a set of {@link ConfigOption} that an implementation of this factory consumes in
+     * addition to {@link #requiredOptions()}.
+     *
+     * <p>See the documentation of {@link Factory} for more information.
+     */
     @Override
     public Set<ConfigOption<?>> optionalOptions() {
         final Set<ConfigOption<?>> options = new HashSet<>();
@@ -55,6 +76,13 @@ public class HttpDynamicTableFactory implements DynamicTableSourceFactory {
         return options;
     }
 
+    /**
+     * Creates a {@link DynamicTableSource} instance from a {@link CatalogTable} and additional
+     * context information.
+     *
+     * <p>An implementation should perform validation and the discovery of further (nested)
+     * factories in this method.
+     */
     @Override
     public DynamicTableSource createDynamicTableSource(Context context) {
         // either implement your custom validation logic here ...
--- flink/src/main/java/cn/rwj/study/flink/connector/http/HttpDynamicTableSource.java
+++ flink/src/main/java/cn/rwj/study/flink/connector/http/HttpDynamicTableSource.java
@@ -4,11 +4,10 @@ import org.apache.flink.api.common.serialization.DeserializationSchema;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.table.connector.ChangelogMode;
 import org.apache.flink.table.connector.format.DecodingFormat;
-import org.apache.flink.table.connector.source.DynamicTableSource;
-import org.apache.flink.table.connector.source.ScanTableSource;
-import org.apache.flink.table.connector.source.SourceFunctionProvider;
+import org.apache.flink.table.connector.source.*;
 import org.apache.flink.table.data.RowData;
 import org.apache.flink.table.types.DataType;
+import org.apache.flink.types.RowKind;
 
 /**
  * HttpDynamicTableSource 实现 ScanTableSource，
@@ -35,6 +34,11 @@ public class HttpDynamicTableSource implements ScanTableSource {
     }
 
 
+    /**
+     * Returns the set of changes that the planner can expect during runtime.
+     *
+     * @see RowKind
+     */
     @Override
     public ChangelogMode getChangelogMode() {
         // in our example the format decides about the changelog mode
@@ -42,6 +46,26 @@ public class HttpDynamicTableSource implements ScanTableSource {
         return decodingFormat.getChangelogMode();
     }
 
+    /**
+     * Returns a provider of runtime implementation for reading the data.
+     *
+     * <p>There might exist different interfaces for runtime implementation which is why {@link
+     * ScanRuntimeProvider} serves as the base interface. Concrete {@link ScanRuntimeProvider}
+     * interfaces might be located in other Flink modules.
+     *
+     * <p>Independent of the provider interface, the table runtime expects that a source
+     * implementation emits internal data structures (see {@link
+     * org.apache.flink.table.data.RowData} for more information).
+     *
+     * <p>The given {@link ScanContext} offers utilities by the planner for creating runtime
+     * implementation with minimal dependencies to internal data structures.
+     *
+     * <p>{@link SourceProvider} is the recommended core interface. {@code SourceFunctionProvider}
+     * in {@code flink-table-api-java-bridge} and {@link InputFormatProvider} are available for
+     * backwards compatibility.
+     *
+     * @see SourceProvider
+     */
     @Override
     public ScanRuntimeProvider getScanRuntimeProvider(ScanContext runtimeProviderContext) {
         // create runtime classes that are shipped to the cluster
@@ -51,11 +75,16 @@ public class HttpDynamicTableSource implements ScanTableSource {
         return SourceFunctionProvider.of(sourceFunction, false);
     }
 
+    /**
+     * Creates a copy of this instance during planning. The copy should be a deep copy of all
+     * mutable members.
+     */
     @Override
     public DynamicTableSource copy() {
         return new HttpDynamicTableSource(url, interval, decodingFormat, producedDataType);
     }
 
+    /** Returns a string that summarizes this source for printing to a console or log. */
     @Override
     public String asSummaryString() {
         return "Http Table Source";
