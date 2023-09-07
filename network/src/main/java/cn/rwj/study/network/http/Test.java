3e4245600cae175447fdec07900790da88427a31
rwj <rwj005@outlook.com>
2023-09-07 09:22:31 +0800

    feature: Apache Common 包的 HttpClient demo；

--- network/src/main/java/cn/rwj/study/network/http/Test.java
+++ network/src/main/java/cn/rwj/study/network/http/Test.java
@@ -8,7 +8,9 @@ package cn.rwj.study.network.http;
  *      1、JDK 原生 java.net.HttpURLConnection；
  *      2、Apache Common 包的 HttpClient；
  *      3、Apache org.apache.httpcomponents 包的 CloseableHttpClient；
- *      4、SpringBoot-RestTemplate；
+ *      4、Spring RestTemplate 或者 WebClient（Spring 5引入，是一个非阻塞的、响应式的Web客户端）
+ *      5、Hutool-http
+ *      6、使用OkHttp
  *
  */
 public class Test {
